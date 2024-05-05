5c578488e603a356ecb801f6e03b75be2093cd60
IT21234484 <IT21234484@my.sliit.lk>
2024-05-05 19:26:38 +0530

    app js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext";
 import { ToastContextProvider } from "./context/ToastContext";
 import Rovers from "./pages/rovers/Rovers";
+import Astronomy from "./pages/Astronomy";
+// import Homepage from "./pages/Homepage";
+import Homepage from "./pages/Homepage";
+import Selection from "./pages/Selection";
+import EarthImages from "./pages/EarthImages";
+import Login from "./pages/Login";
+import Registration from "./pages/Register";
+
+
 
 function App() {
   return (
@@ -15,7 +24,31 @@ function App() {
         <AuthContextProvider>
           <Layout>
             <Routes>
-              <Route path="/" element={<Rovers />} />
+            <Route path="/Login" element={<Login />} />
+            <Route path="/Register" element={<Registration />} />
+            <Route path="/" element={<Homepage />} />
+            <Route path="/Selection" element={<Selection />} />
+            <Route path="/Astronomy" element={<Astronomy />} />
+            <Route path="/EarthImages" element={<EarthImages />} />
+            <Route path="/Rovers" element={<Rovers />} />
+
+
+
+            {/* <Route path="/" element={<Homepage />} />
+            <Route path="/Selection" element={<Selection />} />
+
+              <Route path="/Rovers" element={<Rovers />} />
+              <Route path="/RoversPanoromic" element={<RoversPanoromic />} />
+              <Route path="/Astronomy" element={<Astronomy />} />
+              <Route path="/EarthImages" element={<EarthImages />} />
+              <Route path="/Login" element={<Login />} />
+              <Route path="/Register" element={<Registration />} /> */}
+
+
+
+
+
+
               {/* <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} /> */}
             </Routes>
