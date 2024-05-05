b3630ba0d8956bd005a1f56b27d46864cc8604e4
IT21234484 <IT21234484@my.sliit.lk>
2024-05-05 16:52:44 +0530

    authcontext done

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -11,95 +11,94 @@ export const AuthContextProvider = ({ children }) => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  // useEffect(() => {
-  //   checkUserLoggedIn();
-  // }, []);
+  useEffect(() => {
+    checkUserLoggedIn();
+  }, []);
 
   //check user is logged in
-  // const checkUserLoggedIn = async () => {
-  //   try {
-  //     const res = await fetch(`http://localhost:9000/api/me`, {
-  //       method: "GET",
-  //       headers: {
-  //         Authorization: `Bearer ${localStorage.getItem("token")}`,
-  //       },
-  //     });
-  //     const result = await res.json();
-  //     if (!result.error) {
-  //       if (
-  //         location.pathname === "/login" ||
-  //         location.pathname === "/register"
-  //       ) {
-  //         setTimeout(() => {
-  //           navigate("/", { replace: true });
-  //         }, 500);
-  //       } else {
-  //         navigate(location.pathname ? location.pathname : "/");
-  //       }
-  //       setUser(result);
-  //     } else {
-  //       // navigate("/login", { replace: true });
-  //     }
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
-  // //login request
-  // const loginUser = async (userData) => {
-  //   try {
-  //     const res = await fetch(`http://localhost:9000/api/login`, {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify({ ...userData }),
-  //     });
-  //     const result = await res.json();
-  //     if (!result.error) {
-  //       toast.success("Logged in successfully");
-  //       localStorage.setItem("token", result.jwtToken);
-  //       setUser(result.user);
-
-  //       navigate("/", { replace: true });
-  //     } else {
-  //       toast.error(result.error);
-  //     }
-  //   } catch (err) {
-  //     console.error(err.message);
-  //   }
-  // };
-  // //register request
-  // const registerUser = async (userData) => {
-  //   try {
-  //     const res = await fetch(`http://localhost:9000/api/register`, {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify({ ...userData }),
-  //     });
-  //     const result = await res.json();
-  //     if (!result.error) {
-  //       toast.success("User Registered Successfully! Login to continue ");
-  //       navigate("/login", { replace: true });
-  //     } else {
-  //       toast.error(result.error);
-  //     }
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
+  const checkUserLoggedIn = async () => {
+    try {
+      const res = await fetch(`http://localhost:8002/api/me`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      const result = await res.json();
+      if (!result.error) {
+        setUser(result);
+        if (
+          location.pathname === "/login" ||
+          location.pathname === "/register"
+        ) {
+          setTimeout(() => {
+            navigate("/", { replace: true });
+          }, 500);
+        } else {
+          navigate(location.pathname ? location.pathname : "/");
+        }
+      } else {
+        navigate("/login", { replace: true });
+      }
+    } catch (err) {
+      console.log(err);
+      navigate("/login", { replace: true });
+    }
+  };
+  //login request
+  const loginUser = async (userData) => {
+    try {
+      const res = await fetch(`http://localhost:8002/api/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ ...userData }),
+      });
+      const result = await res.json();
+      if (!result.error) {
+        setUser(result.user);
+        toast.success("Logged in successfully");
+        localStorage.setItem("token", result.jwtToken);
+        navigate("/", { replace: true });
+      } else {
+        toast.error(result.error);
+      }
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+  //register request
+  const registerUser = async (userData) => {
+    try {
+      const res = await fetch(`http://localhost:8002/api/register`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ ...userData }),
+      });
+      const result = await res.json();
+      if (!result.error) {
+        toast.success("User Registered Successfully! Login to continue ");
+        navigate("/login", { replace: true });
+      } else {
+        toast.error(result.error);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
-    <AuthContext.Provider
-    // value={{ loginUser, registerUser, user, setUser }}
-    >
+    <AuthContext.Provider value={{ loginUser, registerUser, user, setUser }}>
       {children}
     </AuthContext.Provider>
   );
 };
 
+export default AuthContext;
+
 AuthContextProvider.propTypes = {
   children: PropTypes.node.isRequired,
 };
-export default AuthContext;
